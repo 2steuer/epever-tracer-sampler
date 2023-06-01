82228eb679564aa44f621325eb9f5e98b75dedfd
Merlin Steuer <mail@merlinsteuer.de>
2023-06-01 14:54:28 +0200

    using big endian

--- Source/EpeverTracerSampler/Sampler/TracerSampler.cs
+++ Source/EpeverTracerSampler/Sampler/TracerSampler.cs
@@ -87,7 +87,7 @@ namespace EpeverTracerSampler.Sampler
 
         public void Start()
         {
-            _clt.Connect(_port, ModbusEndianness.LittleEndian);
+            _clt.Connect(_port);
 
             _timer.Start();
         }

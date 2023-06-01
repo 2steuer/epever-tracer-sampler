7a811f08ba32af75d56576778e39f773eaa1227e
Merlin Steuer <2steuer@informatik.uni-hamburg.de>
2023-06-01 15:52:51 +0200

    - Restructuring register parsing

--- Source/EpeverTracerSampler/Sampler/TracerSampler.cs
+++ Source/EpeverTracerSampler/Sampler/TracerSampler.cs
@@ -39,28 +39,33 @@ namespace EpeverTracerSampler.Sampler
             _timer.Elapsed += Sample;
         }
 
+        private int DecodeDword(short l, short h)
+        {
+            return ((ushort)l | (h << 16));
+        }
+
         private void Sample(object? sender, System.Timers.ElapsedEventArgs e)
         {
             Console.WriteLine("Sampling...");
 
             try
             {
-                var r = _clt.ReadInputRegisters(_id, 0x3100, 18);
+                var r = _clt.ReadInputRegisters<short>(_id, 0x3100, 18);
 
-                                double pvVoltage = BitConverter.ToInt16(r) / 100.0; // 0x3100, 1/100 V
-                double pvCurrent = BitConverter.ToInt16(r.Slice(2)) / 100.0; // 0x3101, 1/100 A
-                double pvPower = BitConverter.ToInt32(r.Slice(4)) / 100.0; // 0x3102, 1/100 W 32 bit
+                double pvVoltage = r[0x00] / 100.0; // 0x3100, 1/100 V
+                double pvCurrent = r[0x01] / 100.0; // 0x3101, 1/100 A
+                double pvPower = DecodeDword(r[0x02], r[0x03]) / 100.0; // 0x3102, 1/100 W 32 bit
 
-                double batteryVoltage = BitConverter.ToInt16(r.Slice(8)) / 100.0; // 0x3104, 1/100V
-                double batteryCurrent = BitConverter.ToInt16(r.Slice(10)) / 100.0; // 0x3105, 1/100A
-                double batteryPower = BitConverter.ToInt32(r.Slice(12)) / 100.0; // 0x3106, 1/100 W 32 bit
+                double batteryVoltage = r[0x04] / 100.0; // 0x3104, 1/100V
+                double batteryCurrent = r[0x05] / 100.0; // 0x3105, 1/100A
+                double batteryPower = DecodeDword(r[0x06], r[0x07]) / 100.0; // 0x3106, 1/100 W 32 bit
 
-                double loadVoltage = BitConverter.ToInt16(r.Slice(0xC * 2)) / 100.0; // 0x310C, 1/100V
-                double loadCurrent = BitConverter.ToInt16(r.Slice(0xD * 2)) / 100.0; // 0x310D, 1/100A
-                double loadPower = BitConverter.ToInt32(r.Slice(0xF * 2)) / 100.0; // 0x310F, 1/100 W 32 bit
+                double loadVoltage = r[0x0C] / 100.0; // 0x310C, 1/100V
+                double loadCurrent = r[0x0D] / 100.0; // 0x310D, 1/100A
+                double loadPower = DecodeDword(r[0x0E], r[0x0F]) / 100.0; // 0x310F, 1/100 W 32 bit
 
-                double batteryTemp = BitConverter.ToInt16(r.Slice(0x10 * 2)) / 100.0; // 0x3110, 1/100 °C
-                double deviceTemp = BitConverter.ToInt16(r.Slice(0x11 * 2)) / 100.0; // 0x3111, 1/100 °C
+                double batteryTemp = r[0x10] / 100.0; // 0x3110, 1/100 °C
+                double deviceTemp = r[0x11] / 100.0; // 0x3111, 1/100 °C
 
                 var s = new TracerSample(DateTime.Now, new()
             {
